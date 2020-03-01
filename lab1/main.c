18eb580fa29794aec4b17bfdda067419fdbf4df3
herreraosmar83 <osmar.herreraperez.945@my.csun.edu>
2020-02-29 20:34:07 -0800

    final changes

--- lab1/main.c
+++ lab1/main.c
@@ -4,71 +4,89 @@
 #include <unistd.h>
 #include <time.h>
 #include <sys/wait.h>
+#include <sys/times.h>
+//differnt functions in program
 int forky();
 int timer();
+int procTimes();
 int main(void){
-    // time in seconds passed since 1970<- i dont know why
+    struct tms end_tms;
     timer(0);
-//create child process here
+    // starting time in seconds passed since 1970<- i dont know why
+    //create child process here
     forky();
-    //print out the process id of parent
-    //print out the process id of child 
-    //printf("\n\nnefunrefnrenf rePPID: %d", (int)getpid());
+    // procTimes();
+    times(&end_tms);
+    clock_t cpu_time = end_tms.tms_cutime;
+    clock_t utime = end_tms.tms_utime;
+    clock_t stime = end_tms.tms_stime;
+    clock_t cstime = end_tms.tms_cstime;
+
+    printf("USER: %ld, ", cpu_time);
+    printf("SYS: %ld \n", utime);
+    printf("CUSER: %ld , ", stime);
+    printf("CSYS: %ld, \n", cstime);
+    // printf("USER: %ld, ", buf.tms_utime); 
+    // printf("SYS: %ld \n", buf.tms_stime); 
+    // printf("CUSER: %ld , ", buf.tms_cutime); 
+    // printf("CSYS: %ld, \n", buf.tms_cstime); 
     
-    printf(", RETVAL: some value");
-
-    printf("\nUSER: some value");
-    printf(", SYS: some value");
-
-    printf("\nCUSER: some value");
-    printf(", CSYS: some value");
-
+    //ENDING TIME of program SINCE 1970~ probably something to do with history
     timer(1);
     return(0); 
 }
 int forky(){
     int status;
     pid_t pid = fork();
-
+    //fork tipically returns 1 or zero for parent or child process and -1 for an error.
     if(pid<0){
         perror("forking Failed");
     }
+    //this is the child process
     else if(pid==0){
-        //this is the child process
-         //pid 
-        printf("\nPID: %d",(int)getpid());
+        //pid 
+        printf("\nPID: %d,",(int)getpid());
         //ppid (parent id)
         printf(" PPID: %d\n",(int)getppid());
         sleep(2);
         exit(EXIT_SUCCESS);
     }
+    //this is the parent process
     else{
-        //this is the parent process
-        waitpid(pid,&status,0);
         //process id 
-        printf("PID: %d",(int)getpid());
+        printf("PID: %d, ",(int)getpid());
         //the process id of parent to parent
-        printf(" PPID: %d ", (int)getppid());
+        printf("PPID: %d, ", (int)getppid());
         //the process id of child (which should be the same as pid of child pid above for case 2)
-        printf("CPID: %d \n",pid);
-
+        printf("CPID: %d, ",pid);
+        waitpid(pid,&status,0);
+        if (WIFEXITED(status)){
+            int returned = WEXITSTATUS(status);
+            printf("RETVAL %d\n",returned);
+        }
+        sleep(1);
     }
     return (0);
 }
 int timer(int val){
-    if(val==0){
-        time_t seconds; 
-        seconds = time(NULL); 
+    time_t seconds; 
+    seconds = time(NULL); 
+    if(val==0){ //starting case
         printf("\nSTART: %ld", seconds);
     }
-    else{
-        time_t seconds; 
-        seconds = time(NULL); 
-        printf("\nSTOP: %ld", seconds);
+    else{//ending case
+        printf("STOP: %ld  \n", seconds);
     }
     return (0);
 }
-
+int procTimes(){
+    
+      /* user time */
+     /* system time */
+     /* user time of children */
+     /* system time of children */
+     return(0);
+}
 
 
 
