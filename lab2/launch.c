212389e5a2f92aa77281e87bd4e2fa7c7c3aa27a
herreraosmar83 <osmar.herreraperez.945@my.csun.edu>
2020-03-12 13:54:50 -0700

    initial stages of lab2: created basic structure

---
+++ lab2/launch.c
@@ -0,0 +1,57 @@
+#include <stdio.h>  
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int forky();
+int main(int args,char *command[]){
+    printf("\nForking to creat a child process:  \n");
+    forky(command);
+    return (0);
+}
+int forky(char *command[]){
+    int status;
+    pid_t pid = fork();
+    //fork tipically returns 1 or zero for parent or child process and -1 for an error.
+    if(pid<0){
+        perror("The forking Failed");
+    }
+    //this is the child process
+    else if(pid==0){
+        //pid 
+     printf("\n child process: \n");
+        // ● The child process executes the supplied command (see execve(2))
+        // ○ the child needs to prepare the new argv structure
+        //take arguemements and execute the supplied arguemmnnts
+        char *args[] = {command[1]};
+        execvp(command[0],args);
+    }//parent process
+    else{
+        // ● The parent process prints the PID of the child on stderr
+        fprintf(stderr, "PID of Child: %d",pid); // Error message on stderr (using fprintf)
+        waitpid(pid,&status,0);
+
+    }
+    
+    
+    //prints out arguemnt 1
+    return (0);
+}
+
+
+
+
+
+
+
+
+
+
+// In this software project, you are to write a command-line tool that launches another process and
+// then have this process execute a particular program. The general steps that this program
+// undergoes include:
+// ● The program forks a child process (see fork(2))
+// ● The parent process prints the PID of the child on stderr
+// ● The child process executes the supplied command (see execve(2))
+// ○ the child needs to prepare the new argv structure
+// ● The parent prints the return value of the child on stderr (see waitpid(2))
---
+++ test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>  
+#include <stdlib.h>
+
+int main(){
+    printf("\n process 2 ran\n");
+    return(0);
+}
