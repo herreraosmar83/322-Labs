12f3e4a6f0cc89be8dd28991a3a3ea183b399e68
herreraosmar83 <osmar.herreraperez.945@my.csun.edu>
2020-05-13 00:12:35 -0700

    completed hopefully

--- lab4/main.c
+++ lab4/main.c
@@ -19,7 +19,8 @@
 bool value = true;
 int philosopher;
 int signalCounter;
-
+void think(int philospher);
+void eat(int philosopher);
 void myhandle(int signum) {
     //reregister handled signal
     value = false;
@@ -27,51 +28,57 @@ void myhandle(int signum) {
     seconds = time(NULL); 
     printf("caught SIGETERM at %ld seconds\n",seconds);
     fprintf(stderr, "Philosopher #%d had %d turns\n", philosopher, signalCounter);
-
     exit(EXIT_SUCCESS);
     
 }
 
 int main(int argc, char *argv[]){   
-    //assume if more than two arguements then a sigterm command has been entered so program will execute termination 
-    // if(argc > 2)
-    philosopher = argv[1];
-    int value1;
-    int value2;
     pid_t pid = getpid();
-    sem_t *chopstick_1 = sem_open(SEM_FILE1, O_CREAT, 0666, 1);
-    sem_t *chopstick_2 = sem_open(SEM_FILE2, O_CREAT, 0666, 1);
-    fprintf(stderr,"\nPID: %d \n",(int)pid);
-    sem_getvalue(chopstick_1, &value1);
-    sem_getvalue(chopstick_2, &value2);
-    signal(SIGTERM,myhandle);
-    while (value){
+    //assume if kill has been entered so program will execute termination 
+    if(strcmp(argv[1],"kill") == 0){
+         //kills process hopefully 
+        int pidToKill = atoi(argv[3]);
+        kill(pidToKill,SIGTERM);
+        printf("value %s",argv[1]);
+   
+    }
+    else{
+        philosopher = atoi(argv[1]); //atoi to cast properly
+        int value1;
+        int value2;
+        sem_t *chopstick_1 = sem_open(SEM_FILE1, O_CREAT, 0666, 1);
+        sem_t *chopstick_2 = sem_open(SEM_FILE2, O_CREAT, 0666, 1);
+        fprintf(stderr,"\nPID: %d \n",(int)pid);
+        sem_getvalue(chopstick_1, &value1);
+        sem_getvalue(chopstick_2, &value2);
+        signal(SIGTERM,myhandle);
+        while (value){
+            sem_wait(chopstick_1);
+            sem_wait(chopstick_2);
+            // philosopher should be eating 
+            eat(philosopher);
         
-        sem_wait(chopstick_1);
-      
-        sem_wait(chopstick_2);
-        // philosopher should be eating 
-        eat(philosopher);
-      
-        sem_post(chopstick_1);
-        sem_post(chopstick_2);
-        // philosopher should be thinking
-        think(philosopher);
-        signalCounter++;
-    };
-    sem_close(chopstick_1);
-    sem_close(chopstick_2);
-    sem_unlink(SEM_FILE1);
-    sem_unlink(SEM_FILE2);
+            sem_post(chopstick_1);
+            sem_post(chopstick_2);
+            // philosopher should be thinking
+            think(philosopher);
+            signalCounter++;
+        };
+        sem_close(chopstick_1);
+        sem_close(chopstick_2);
+        sem_unlink(SEM_FILE1);
+        sem_unlink(SEM_FILE2);    
+    }
+    
     return (0);
 }
-void think(int phil_num){
-    printf("Philosopher #%d is thinking\n", phil_num);
-    usleep(rand() % 5);
+void think(int philosopher){
+    printf("Philosopher #%d is thinking\n", philosopher);
+    usleep(rand() % 5000000);
 }
-void eat(int phil_num){
-    printf("Philosopher #%d is eating\n", phil_num);
-    usleep(rand() % 5);
+void eat(int philosopher){
+    printf("Philosopher #%d is eating\n", philosopher);
+    usleep(rand() % 5000000);
 }
 
 //semaphore chopstick [5];
